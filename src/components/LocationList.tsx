69820c590f51ba32a5b291129f8534ecb8860836
mohammed884 <mohammed.knite25@gmail.com>
2024-07-06 07:51:56 +0300

    remove unecceserly packages

--- src/components/LocationList.tsx
+++ src/components/LocationList.tsx
@@ -1,7 +1,26 @@
 import React from "react";
-
-const LocationList = () => {
-  return <div>LocationList</div>;
+import {
+  Card,
+  CardTitle,
+  CardHeader,
+  CardContent,
+  CardFooter,
+} from "../shadcn-components/ui/card";
+import { Button } from "../shadcn-components/ui/button";
+const LocationList = ({ title }: { title: string }) => {
+  return (
+    <Card className="w-full flex items-center justify-between">
+      <CardHeader className="sr-only">
+        <CardTitle></CardTitle>
+      </CardHeader>
+      <CardContent className="flex items-center justify-center bg-red-50">
+        <p>{title}</p>
+        <span>Icon</span>
+      </CardContent>
+      <CardFooter className="flex items-center justify-between">
+        <Button variant={"destructive"}>Delete</Button>
+      </CardFooter>
+    </Card>
+  );
 };
-
 export default LocationList;
--- src/pages/Locations.tsx
+++ src/pages/Locations.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 import { Button } from "../shadcn-components/ui/button";
-import { Separator } from "../shadcn-components/ui/Separator";
+import { Separator } from "../shadcn-components/ui/separator";
+import LocationList from "../components/LocationList";
 const Locations = () => {
+  const dummy = [
+    "location one",
+    "location one",
+    "location one",
+    "location one",
+    "location one",
+  ];
   return (
     <section className="pt-[10rem]">
       <div className="container mx-auto px-4 md:px-6">
@@ -9,8 +17,13 @@ const Locations = () => {
           Browse and add your locations !
         </h1>
         <Button>Add new location</Button>
+        <Separator className="mt-8" />
+        <ul>
+          {dummy.map((title: string) => (
+            <LocationList title={title} />
+          ))}
+        </ul>
       </div>
-      <Separator />
     </section>
   );
 };
