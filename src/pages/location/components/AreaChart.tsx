31f3190b08ea3fdef4cdf7579901adc9a3bd9642
z7pz <sdfygamer@gmail.com>
2024-07-09 20:17:45 +0300

    fix: bug fixes

--- src/pages/location/components/AreaChart.tsx
+++ src/pages/location/components/AreaChart.tsx
@@ -77,26 +77,26 @@ const AreaChart = ({ data, config, chartKey, setChartKey }: any) => {
             />
             <ChartTooltip cursor={false} content={<ChartTooltipContent />} />
             <defs>
-              <linearGradient id="fillAQI" x1="0" y1="0" x2="0" y2="1">
+              <linearGradient id={"fill"+chartKey} x1="0" y1="0" x2="0" y2="1">
                 <stop
                   offset="5%"
-                  stopColor="var(--color-AQI)"
+                  stopColor={`var(--color-${chartKey})`}
                   stopOpacity={0.8}
                 />
                 <stop
                   offset="95%"
-                  stopColor="var(--color-AQI)"
+                  stopColor={`var(--color-${chartKey})`}
                   stopOpacity={0.1}
                 />
               </linearGradient>
             </defs>
 
             <Area
-              dataKey="AQI"
+              dataKey={chartKey}
               type="natural"
-              fill="url(#fillAQI)"
+              fill={`url(#fill${chartKey})`}
               fillOpacity={0.4}
-              stroke="var(--color-AQI)"
+              stroke={`var(--color-${chartKey})`}
               stackId="a"
             />
           </ShadCnAreaChart>
