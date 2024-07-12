cbc11150f3339b36310323db48abafcc05c36ca9
mohammed884 <mohammed.knite25@gmail.com>
2024-07-12 21:29:47 +0300

    add static location

--- src/pages/locations/public/index.tsx
+++ src/pages/locations/public/index.tsx
@@ -57,10 +57,34 @@ const Locations = () => {
               </li>
             ))}
             {!publicLocations.data?.length && (
-              <li>
-                <span className="text-sm font-bold opacity-50">
-                  لا توجد مواقع عامة حاليا
-                </span>
+              <li
+                key={"1"}
+                className="flex justify-between gap-x-6 py-5 hover:bg-gray-100 hover:dark:bg-gray-800 px-5 rounded cursor-pointer transition-colors"
+              >
+                <div className="flex min-w-0 gap-x-4">
+                  <div className="min-w-0 flex  items-center gap-5">
+                    <Link key={"1"} to={`/locations/public/2`}>
+                      <div className="flex items-center gap-3">
+                        <p className="text-sm font-semibold leading-6 text-gray-900 dark:text-gray-200">
+                          Baghdad Iraq
+                        </p>
+                      </div>
+                    </Link>
+                  </div>
+                </div>
+                <Link key={1} to={`/locations/public/${2}`}>
+                  <div className="shrink-0 flex flex-row items-end gap-5">
+                    <div className="mt-1 flex items-center gap-x-1.5 h-full">
+                      <div className="flex-none rounded-full bg-emerald-500/20 p-1 animate-pulse">
+                        <div className="h-1.5 w-1.5 rounded-full bg-emerald-500" />
+                      </div>
+                      <p className="text-xs leading-5 text-gray-500 dark:text-gray-400">
+                        الاجهزة المتصلة
+                        <span> ({1})</span>
+                      </p>
+                    </div>
+                  </div>
+                </Link>
               </li>
             )}
           </ul>
